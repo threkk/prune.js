26d75620bd95d71620e32cc869d56c205938816d
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2017-07-01 20:14:25 +0200

    Literal new syntax.

--- src/code/literal.js
+++ src/code/literal.js
@@ -1,46 +1,175 @@
-const { Node, parse } = require('./trace')
+const AbstractNode = require('../abstract/node')
+const parse = require('./parse')
 
-function parseLiteral (node) {
-  const id = []
-  const type = {
-    main: 'literal',
-    sub: node.type
+/**
+ *  interface Identifier <: Expression, Pattern {
+ *      type: "Identifier";
+ *      name: string;
+ *  }
+ */
+class Identifier extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+    this.returns = node.name
   }
-  const loc = node.loc
-  const declares = []
-  let uses = []
-  const children = []
-
-  switch (node.type) {
-    case 'RegExpLiteral':
-    case 'NullLiteral':
-    case 'StringLiteral':
-    case 'BooleanLiteral':
-    case 'NumericLiteral':
-    case 'TemplateElement':
-      break
-    case 'TemplateLiteral':
-      node.quasis.forEach((q) => {
-        const quasi = parse(q).uses
-        uses = uses.concat(quasi)
-      })
-
-      node.expressions.forEach((expr) => {
-        const expression = parse(expr).uses
-        uses = uses.concat(expression)
-      })
-      break
-    case 'TaggedTemplateExpression':
-      if (node.tag != null) {
-        uses = uses.concat(parse(node.tag).uses)
-      }
-
-      if (node.quasi != null) {
-        uses = uses.concat(parse(node.quasi).uses)
-      }
-      break
-  }
-  return new Node(id, type, loc, declares, uses, children)
 }
 
-module.exports = parseLiteral
+/**
+ *  interface PrivateName <: Expression, Pattern {
+ *      type: "PrivateName";
+ *      name: Identifier;
+ *  }
+ */
+class PrivateName extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+    const name = parse(node.name)
+    this.returns = name.returns
+  }
+}
+
+/**
+ * interface Literal <: Expression { }
+ */
+class Literal extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface RegExpLiteral <: Literal {
+ *      type: "RegExpLiteral";
+ *      pattern: string;
+ *      flags: string;
+ * }
+ */
+class RegExpLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface NullLiteral <: Literal {
+ *      type: "NullLiteral";
+ *  }
+ */
+class NullLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface StringLiteral <: Literal {
+ *      type: "StringLiteral";
+ *      value: string;
+ *  }
+ */
+class StringLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface BooleanLiteral <: Literal {
+ *      type: "BooleanLiteral";
+ *      value: boolean;
+ *  }
+ */
+class BooleanLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface NumericLiteral <: Literal {
+ *      type: "NumericLiteral";
+ *      value: number;
+ *  }
+ */
+class NumericLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+/**
+ *  interface TemplateLiteral <: Expression {
+ *      type: "TemplateLiteral";
+ *      quasis: [ TemplateElement ];
+ *      expressions: [ Expression ];
+ *  }
+ */
+class TemplateLiteral extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+    const quasis = node.quasis.map((q) => parse(q))
+    const expressions = node.expressions.map((e) => parse(e))
+
+    // TODO: review this.
+    expressions.forEach((exp) => {
+      this.uses = exp.uses
+      this.uses = exp.returns
+    })
+
+    quasis.forEach((quasi) => {
+      this.uses = quasi.uses
+      this.uses = quasi.returns
+    })
+  }
+}
+
+/**
+ *  interface TaggedTemplateExpression <: Expression {
+ *      type: "TaggedTemplateExpression";
+ *      tag: Expression;
+ *      quasi: TemplateLiteral;
+ *  }
+ */
+class TaggedTemplateExpression extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.constructor)
+    const tag = parse(node.tag)
+    const quasi = parse(node.quasi)
+
+    this.uses = tag.returns
+    this.uses = tag.uses
+
+    this.uses = quasi.returns
+    this.uses = quasi.uses
+  }
+}
+
+/**
+ *  interface TemplateElement <: Node {
+ *      type: "TemplateElement";
+ *      tail: boolean;
+ *      value: {
+ *          cooked: string | null;
+ *          raw: string;
+ *      };
+ *  }
+ */
+class TemplateElement extends AbstractNode {
+  constructor (node) {
+    super(node.loc, node.type)
+  }
+}
+
+module.exports = {
+  Identifier,
+  PrivateName,
+  Literal,
+  RegExpLiteral,
+  NullLiteral,
+  StringLiteral,
+  BooleanLiteral,
+  NumericLiteral,
+  TemplateLiteral,
+  TaggedTemplateExpression,
+  TemplateElement
+}
--- src/code/parse.js
+++ src/code/parse.js
@@ -1,32 +1,71 @@
-const {
-  Class,
-  ClassBody,
-  ClassMethod,
-  ClassProperty,
-  ClassPrivateProperty,
-  ClassDeclaration,
-  ClassExpression,
-  MetaProperty
-} = require('./class')
-
 function parse (node) {
+  const {
+    Class,
+    ClassBody,
+    ClassMethod,
+    ClassProperty,
+    ClassPrivateProperty,
+    ClassDeclaration,
+    ClassExpression,
+    MetaProperty
+  } = require('./class')
+
+  const {
+    Identifier,
+    PrivateName,
+    Literal,
+    RegExpLiteral,
+    NullLiteral,
+    StringLiteral,
+    BooleanLiteral,
+    NumericLiteral,
+    TemplateLiteral,
+    TaggedTemplateExpression,
+    TemplateElement
+  } = require('./literal')
+
   switch (node.type) {
-    case Class:
+    // class.js
+    case 'Class':
       return new Class(node)
-    case ClassBody:
+    case 'ClassBody':
       return new ClassBody(node)
-    case ClassMethod:
+    case 'ClassMethod':
       return new ClassMethod(node)
-    case ClassProperty:
+    case 'ClassProperty':
       return new ClassProperty(node)
-    case ClassPrivateProperty:
+    case 'ClassPrivateProperty':
       return new ClassPrivateProperty(node)
-    case ClassDeclaration:
+    case 'ClassDeclaration':
       return new ClassDeclaration(node)
-    case ClassExpression:
+    case 'ClassExpression':
       return new ClassExpression(node)
-    case MetaProperty:
+    case 'MetaProperty':
       return new MetaProperty(node)
+
+    // literal.js
+    case 'Identifier':
+      return new Identifier(node)
+    case 'PrivateName':
+      return new PrivateName(node)
+    case 'Literal':
+      return new Literal(node)
+    case 'RegExpLiteral':
+      return new RegExpLiteral(node)
+    case 'NullLiteral':
+      return new NullLiteral(node)
+    case 'StringLiteral':
+      return new StringLiteral(node)
+    case 'BooleanLiteral':
+      return new BooleanLiteral(node)
+    case 'NumericLiteral':
+      return new NumericLiteral(node)
+    case 'TemplateLiteral':
+      return new TemplateLiteral(node)
+    case 'TaggedTemplateExpression':
+      return new TaggedTemplateExpression(node)
+    case 'TemplateElement':
+      return new TemplateElement(node)
   }
 }
 
