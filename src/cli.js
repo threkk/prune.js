95c695b42523d8b45fbc44c92c74c2943c8985a7
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2018-10-24 01:04:34 +0200

    refactor: simplifies the CLI

--- src/cli.js
+++ src/cli.js
@@ -1,76 +1,31 @@
 #!/usr/bin/env node --harmony
 const pkg = require('../package.json')
 const program = require('commander')
-const { exit } = require('process')
 const { resolve } = require('path')
 
-const ErrorReport = require('./project/report/error')
 const Logger = require('./project/logger')
 const Project = require('./project/project')
-
-// const CodeAnalyser = require('./code/analyser')
-const DependenciesAnalyser = require('./dependencies/analyser')
-const ModulesAnalyser = require('./modules/analyser')
-
-// Default configuration.
-const config = {
-  path: '.',
-  ignore: [],
-  es7: false,
-  jsx: false
-}
-
-// Initialise the logger.
-const logger = new Logger()
 
 // Sets the CLI.
 program
   .version(pkg.version)
   .description(pkg.description)
-  .usage('[options] <path>')
-  .option('-i, --ignore', 'excludes the following folders. "node_modules" is always ignored.')
-  .option('-x, --jsx', 'adds JSX syntax support.')
-  .option('-7, --es7', 'adds ES7 support.')
-  .arguments('<path...>')
+  .usage('[options]')
+  .option('-p, --path <root>', 'path to execute prunejs', process.cwd())
+  .option('-x, --jsx', 'enables JSX syntax support')
+  .option('-i, --ignore <paths>', 'excludes the following folders', (val) => val.split(','), ['node_modules'])
   .parse(process.argv)
 
-// Hacking a bit the library. This turns the last argument into a variadic
-// argument. That way we can use it to transform the option `ignore` into a
-// list of ignored files (all but the last one) instead of
-if (program.args.length < 1) {
-  const error = new ErrorReport('input', 'Missing path to project.')
-  logger.report(error)
+const { jsx, path, ignore } = program
+
+// Initialise the configuration.
+const config = {
+  root: path,
+  ignore: [...new Set(['node_modules', ...ignore])].map(route => resolve(path, route)),
+  jsx: !!jsx,
+  logger: new Logger(),
+  noColor: Boolean(process.env.NO_COLOR) || false
 }
 
-config.path = program.args.pop()
-config.ignore.push(resolve(config.path, './node_modules'))
-
-if (program.es7) config.es7 = true
-if (program.jsx) config.jsx = true
-if (program.ignore) {
-  if (program.args.length > 0) {
-    program.args.forEach(arg => config.ignore.push(resolve(config.path, arg)))
-  } else {
-    const error = new ErrorReport('input', 'ignore flag enabled but no paths provided.')
-    logger.report(error)
-  }
-}
-
-// Exit if there are errors.
-if (logger.hasErrors(ErrorReport.symbol())) {
-  logger.displayErrors()
-  exit(1)
-}
-
-// Initalises the project.
-try {
-  const project = new Project(config, logger)
-  project
-    .analyse(DependenciesAnalyser)
-    .analyse(ModulesAnalyser)
-    // .analyse(CodeAnalyser)
-    .execute()
-} catch (e) {
-  logger.displayErrors()
-  exit(1)
-}
+const project = new Project(config)
+project.analyse()
