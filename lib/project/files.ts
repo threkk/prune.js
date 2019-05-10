bb40762687e8979d8d876b65e23b0fa9739fe433
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2019-05-10 02:21:34 +0200

    feat: Refactor file finder for a more efficient solution.

--- lib/project/files.ts
+++ lib/project/files.ts
@@ -1,67 +1,25 @@
-import fs, { PathLike } from 'fs'
-import { extname, resolve } from 'path'
-import { promisify } from 'util'
-import log from './logger'
-
-const stat = promisify(fs.stat)
-const readdir = promisify(fs.readdir)
-
-// Windows? ¯\_(ツ)_/¯
-const isHidden: (file: PathLike) => boolean = file =>
-  (file as string).charAt(0) === '.'
-const validExtension: (
-  extensions: string[]
-) => (file: PathLike) => boolean = extensions => file =>
-  extensions.includes(extname(file as string))
-const ignoreDirs: (
-  dirs: PathLike[]
-) => (file: PathLike) => boolean = dirs => file => dirs.includes(file as string)
+import { PathLike } from 'fs'
+import globby from 'globby'
+import { join, resolve } from 'path'
 
 /**
  * Extracts all the file paths from the project. Valid files depend on the
  * configuration. `node_modules` are always ignored.
  */
 export default async function extractFiles(
-  path: PathLike,
-  ignored: PathLike[],
+  path: string,
+  ignored: string[],
   extensions: string[]
 ): Promise<PathLike[]> {
-  const dirs: PathLike[] = [path]
-  const files: string[] = []
+  // Paths to filter in the format: !(p1|p2|...)
+  const pathsToFilter: string[] = ['**/node_modules/**']
+  pathsToFilter.push(...ignored)
+  const filterExpr: string = `!(${pathsToFilter.join('|')})`
 
-  const isIgnored = ignoreDirs(ignored)
-  const isValidExtension = validExtension(extensions)
+  // Any file with a valid expression: **/*.ext
+  const matchExprs: string[] = extensions.map(e => `**/*.${e}`)
+  matchExprs.push(filterExpr)
 
-  // While we have directories to check.
-  while (dirs.length > 0) {
-    // Get the last one.
-    const dir: PathLike = dirs.pop()
-    try {
-      // Get all the elements in the directory.
-      const items: string[] = await readdir(dir)
-
-      // Resolve the paths and filter ignore or hidden elements.
-      const visibleItems: string[] = items
-        .map(item => resolve(dir as string, item))
-        .filter(item => !isIgnored(item) && !isHidden(item))
-
-      // If it is a directory, add it to the list to check.
-      // If it is a file, add it to the resolved paths.
-      for (const itemPath of visibleItems) {
-        try {
-          const stats = await stat(itemPath)
-          if (stats.isDirectory()) {
-            dirs.push(itemPath)
-          } else if (isValidExtension(itemPath)) {
-            files.push(itemPath)
-          }
-        } catch (e) {
-          log.exception(e)
-        }
-      }
-    } catch (e) {
-      log.exception(e)
-    }
-  }
-  return files
+  const matches: string[] = await globby(matchExprs, { gitignore: true })
+  return matches.map(m => resolve(join(path, m)))
 }
