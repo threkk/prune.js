26946ae024ead77cf38af1e9fb8904ca78d44f28
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2021-08-04 02:01:50 +0200

    feat: Updates the cli to always use a config file

--- bin/cli.ts
+++ bin/cli.ts
@@ -3,35 +3,53 @@ import Project from '../lib/project'
 import DeadCode from '../lib/dead-code'
 import { resolve, join, isAbsolute } from 'path'
 
-const [cmd, root, ...entryPoints] = process.argv.slice(2)
+const [cmd, configPath, file] = process.argv.slice(2)
 
-switch (cmd) {
-  case 'dead-code':
-    deadCode(root, entryPoints)
-    break
-  case 'project':
-    project(root)
-    break
-  case 'load':
-    loadConfig(root)
-    break
-  case 'file':
-    const [file, ..._] = entryPoints
-    graph(root, file)
-    break
-  default:
-    console.log('Commands: dead-code, file, load, projects')
+interface ConfigProps {
+  root: string
+  entryPoints: string[]
+  ignore?: string[]
+  isLibrary?: boolean
 }
 
-function deadCode(
-  root: string,
-  entryPoints: string[],
-  ignore: string[] = [],
-  isLibrary: boolean = false
-): void {
-  const project = new Project({ root, ignore, isLibrary })
+const configRoot = resolve(join(process.cwd(), configPath))
+import(configRoot)
+  .then((config: ConfigProps) => {
+    const root: string = isAbsolute(config.root)
+      ? config.root
+      : resolve(join(configRoot, config.root))
+
+    const ignore: string[] = (config.ignore ?? []).map((i) =>
+      isAbsolute(i) ? i : resolve(join(root, i))
+    )
+    const entryPoints: string[] = config.entryPoints.map((e) =>
+      isAbsolute(e) ? e : resolve(join(root, e))
+    )
+    const isLibrary: boolean = config.isLibrary ?? false
+
+    switch (cmd) {
+      case 'dead-code':
+        deadCode({ root, ignore, entryPoints, isLibrary })
+        break
+      case 'project-graph':
+        project({ root, ignore, entryPoints, isLibrary })
+        break
+      case 'file-graph':
+        graph({ root, ignore, entryPoints, isLibrary }, file)
+        break
+      default:
+        console.log('Commands: dead-code, file-graph, project-graph')
+    }
+  })
+  .catch((e) => {
+    console.error(`Configuration load failed: ${e.message}`)
+    console.error(e)
+  })
+
+function deadCode(props: ConfigProps): void {
+  const project = new Project(props)
   const deadCode = new DeadCode(project)
-  for (const entryPoint of entryPoints) {
+  for (const entryPoint of props.entryPoints) {
     deadCode.createSubgraph(entryPoint)
   }
 
@@ -48,53 +66,21 @@ function deadCode(
   const modules = deadCode.getDeadModules().map((d) => d.path)
   console.log('=> Modules', modules.length, modules)
 
-  const statements = deadCode.getDeadStatements().map((d) => d.toString())
+  const statements = deadCode
+    .getDeadStatements()
+    .map((d) => d.toString().slice(1, -1))
   console.log('=> Vertices', statements.length, statements)
 }
 
-function project(root: string): void {
-  const project = new Project({
-    root,
-    ignore: ['dist.js', './test', 'bechmark.js', './media', './examples'],
-  })
+function project(props: ConfigProps): void {
+  const project = new Project(props)
   console.log(project.toString())
 }
 
-function graph(root: string, file: string): void {
-  const path = resolve(join(process.cwd(), file))
-  const project = new Project({ root, ignore: ['dist.js'] })
+function graph(props: ConfigProps, file: string): void {
+  const path = isAbsolute(file) ? file : resolve(join(process.cwd(), file))
+  const project = new Project(props)
   const source = project.files[path]
   if (source) console.log(source.graph.toString())
   else console.log(`${path} not found`)
-}
-
-interface ConfigProps {
-  root: string
-  entryPoints: string[]
-  ignore?: string[]
-  isLibrary?: boolean
-}
-
-function loadConfig(path: string): void {
-  const configRoot = resolve(join(process.cwd(), path))
-  import(configRoot)
-    .then((config: ConfigProps) => {
-      const root: string = isAbsolute(config.root)
-        ? config.root
-        : resolve(join(configRoot, config.root))
-
-      const ignore: string[] = (config.ignore ?? []).map((i) =>
-        isAbsolute(i) ? i : resolve(join(root, i))
-      )
-      const entryPoints: string[] = config.entryPoints.map((e) =>
-        isAbsolute(e) ? e : resolve(join(root, e))
-      )
-
-      const isLibrary: boolean = config.isLibrary ?? false
-      deadCode(root, entryPoints, ignore, isLibrary)
-    })
-    .catch((e) => {
-      console.error(`Configuration load failed: ${e.message}`)
-      console.error(e)
-    })
 }
